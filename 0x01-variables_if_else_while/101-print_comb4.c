21444d579d6feb4256bf4119b5c68682a5677042
JasmondWorks <obafemilared@gmail.com>
2023-02-18 05:19:13 +0000

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int fDig, sDig, tDig;
+
+    for (fDig = 48; fDig <= 55; fDig++)
+    {
+        for (sDig = fDig + 1; sDig <= 56; sDig++)
+        {
+            for (tDig = fDig + 2; tDig <= 57; tDig++)
+            {
+                putchar(fDig);
+                putchar(sDig);
+                putchar(tDig);
+                
+                if (fDig != 55)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+
+    }
+    putchar('\n');
+
+    return (0);
+}
