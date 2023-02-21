1be166e4c8a9ee1a4ab40d9bf8f253402903852c
JasmondTheGreat <obafemiolorede@gmail.com>
2023-02-21 16:55:00 +0000

    update

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include "includes.c"
+
+/**
+ * main - prints Fibonacci numbers
+ * prints the first 98 Fibonacci numbers, starting from 1 and 2,
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, length;
+	long int prevTwoNum, prevNum, nextNum;
+
+	prevTwoNum = 1, prevNum = 2, length = 98;
+
+	printf("%ld, %ld, ", prevTwoNum, prevNum);
+
+	for (i = 3; i <= length; i++)
+	{
+		nextNum = prevNum + prevTwoNum;
+		prevTwoNum = prevNum;
+		prevNum = nextNum;
+
+		if (i < length)
+			printf("%ld, ", nextNum);
+		else
+			printf("%ld", nextNum);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
