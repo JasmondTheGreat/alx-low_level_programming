5f06b37b5202afbe9a65896cb80403e015c9687c
JasmondTheGreat <obafemiolorede@gmail.com>
2023-02-27 15:44:57 +0000

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "includes.c"
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string parameter
+ */
+
+void puts_half(char *str)
+{
+	int length, i, middle;
+
+	length = strlen(str);
+	middle = length / 2;
+
+	for (i = (middle); i < length; i++)
+	{
+		/* if (length % 2 != 0) */
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
