574ff38689766febc4a7ff92bb590705f8a1333c
JasmondTheGreat <obafemiolorede@gmail.com>
2023-02-28 20:44:23 +0000

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,17 +12,17 @@ void puts_half(char *str)
 
 	length = strlen(str);
 	middle = length / 2;
-		
-		if (length % 2 == 0)
-		{
-			for (i = middle; i < length; i++)
-				_putchar(str[i]);
-		}
-		else
-		{
-			for (i = (middle + 1); i < length; i++)
-				_putchar(str[i]);
-		}
+
+	if (length % 2 == 0)
+	{
+		for (i = middle; i < length; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (middle + 1); i < length; i++)
+			_putchar(str[i]);
+	}
 
 		_putchar('\n');
 }
