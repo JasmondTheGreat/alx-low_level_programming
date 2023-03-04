664c19b0a21ca0777b42ee4e6db1396a35fb2002
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-04 21:29:56 +0000

    update

--- 0x06-pointers_arrays_strings/3-strcmp_copy.c
+++ 0x06-pointers_arrays_strings/3-strcmp_copy.c
@@ -10,18 +10,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, len1, result;
+	int i, len1, count1, count2;
 
+	count1 = 0;
+	count2 = 0;
 	len1 = strlen(s1);
 
 	for (i = 0; i < len1; i++)
 	{
-		result =
-			s1[i] > s2[i] ? 15
-			: s1[i] < s2[i] ? -15
-			: 0;
-		break;
+		if (s1[i] < s2[i])
+			count2++;
+		else if (s1[i] > s2[i])
+			count1++;
 	}
 
-	return (result);
+	if (count1 > count2)
+		return (15);
+	else if (count1 < count2)
+		return (-15);
+	else
+		return (0);
 }
