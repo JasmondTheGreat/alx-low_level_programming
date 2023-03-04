877681f12a18616ebd50fb7079d9bdb077854f68
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-04 21:57:09 +0000

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,3 @@
-#include "includes.c"
-
 /**
  * _strcmp - compares two strings
  * @s1: first string
@@ -11,6 +9,8 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, result;
+
+	result = 0;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
