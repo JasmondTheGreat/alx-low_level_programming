ea08b34bed650a91afa469b4c743aae1ae94310b
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-04 20:03:09 +0000

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "includes.c"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 15 if s1 > s2, -15 if s1 < s22, or 0 if they're equal
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, len1, count1, count2;
+
+	count1 = 0;
+	count2 = 0;
+	len1 = strlen(s1);
+
+	for (i = 0; i < len1; i++)
+	{
+		if (s1[i] < s2[i])
+			count2++;
+		else if (s1[i] > s2[i])
+			count1++;
+	}
+
+	if (count1 > count2)
+		return (15);
+	else if (count1 < count2)
+		return (-15);
+	else
+		return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *leet(char *);
-
+char *rot13(char *);
+int _strcmp(char *s1, char *s2);
 
 #endif
