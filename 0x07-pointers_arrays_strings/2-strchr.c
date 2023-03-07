2c49d95620bb2799b917798de3e7ab26f129b5bf
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-07 00:06:52 +0000

    update

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: string to check
+ * @c: character to locate
+ * Return: pointer to the first occurence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			break;
+	}
+
+	return (&s[i]);
+}
