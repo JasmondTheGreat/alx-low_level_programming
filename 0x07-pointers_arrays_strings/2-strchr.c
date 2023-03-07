2f4e9aae133c879eda355bb2b74c779dba54f296
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-07 00:13:00 +0000

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
+		{
+			j = i;
 			break;
+		}
 	}
 
-	return (&s[i]);
+	return (&s[j]);
 }
