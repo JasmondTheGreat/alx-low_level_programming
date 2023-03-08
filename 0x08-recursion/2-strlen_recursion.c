0f1114f5b0d8afd06373c62ae5d99c4f42ad12db
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-08 23:02:22 +0000

    update

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string param
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+	if (s != '\0')
+	{
+		count++;
+
+	}
+}
