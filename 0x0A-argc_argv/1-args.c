aa99876a3779ad6626e1da571854403470c6d124
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-10 19:27:22 +0000

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: argument count
+ * @argv: argument vector (pointer to an array of characters)
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: argument count
+ * @argv: argument vector (pointer to an array of characters)
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
