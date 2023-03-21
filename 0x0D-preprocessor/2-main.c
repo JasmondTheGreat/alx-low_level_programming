340617bb167ccacfdebdf301218d81f3441d2d45
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-21 04:42:21 +0000

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * Prints the name of the file using the __FILE__ predefined macro
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
