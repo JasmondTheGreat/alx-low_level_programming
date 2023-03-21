943b40c25e791d9b06bf93f5760845e64705ce15
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-21 04:32:50 +0000

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * struct dog - a dog structure
+ * @name: member name (a string)
+ * @age: member age (a number)
+ * @owner: member owner (a string)
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
