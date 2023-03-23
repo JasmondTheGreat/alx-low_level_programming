8b0cf6d878d0da726ad1795ecff08c87263de80f
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-23 22:30:26 +0000

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -11,5 +12,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
