bbdebb07111e4f43e3290ed7fd045ddd7c1b5275
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-23 22:27:44 +0000

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: name param to print
+ * @f: function pointer param
+ */
+
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
