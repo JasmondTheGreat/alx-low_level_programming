e3ce6460e93e3b8f32ff4ac035bdd81b67ce665a
JasmondTheGreat <obafemiolorede@gmail.com>
2023-03-24 09:31:54 +0000

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,8 +24,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 			if ((i + 1) < (n) && separator)
 				printf("%s", separator);
-			else
-				printf(" ");
 		}
 		printf("\n");
 
