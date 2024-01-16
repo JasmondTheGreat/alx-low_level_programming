7e46e9413391f97b4a10111a62b8af768feeeb08
JasmondTheGreat <obafemiolorede@gmail.com>
2024-01-16 23:42:51 +0000

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,33 +12,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t length1 = strlen(s1), length2 = strlen(s2);
+	size_t length1 = strlen(s1);
+	size_t length2 = strlen(s2);
 	size_t length = length1 + length2 + 1, i;
 	char *newString = (char *)malloc(sizeof(char) * length);
 
 	if (s1 == NULL)
-	{
-		s1 = (char *)malloc(1);
-
-		s1[0] = '\0';
-	}
+		s1 = "";
 
 	if (s2 == NULL)
-	{
-		s2 = (char *)malloc(1);
-
-		s2[0] = '\0';
-	}
+		s2 = "";
 
 
 	if (newString == NULL)
 		return (NULL);
 
-	for (i = 0; i < length1; i++)
-		newString[i] = s1[i];
+	if (newString != NULL)
+	{
+		for (i = 0; i < length1; i++)
+			newString[i] = s1[i];
 
-	for (i = 0; i < length2; i++)
-		newString[i + length1] = s2[i];
+		for (i = 0; i < length2; i++)
+			newString[i + length1] = s2[i];
+
+	}
 
 	return (newString);
 
