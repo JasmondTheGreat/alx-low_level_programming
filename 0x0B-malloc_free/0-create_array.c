b4e88ffa662f68e6aa23592bf14c43048996d2fc
JasmondTheGreat <obafemiolorede@gmail.com>
2024-01-16 15:25:43 +0000

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,17 @@
 char *create_array(unsigned int size, char c)
 {
 	size_t i;
-	char* array = malloc(sizeof(char) * size);
+	char *array = malloc(sizeof(char) * size);
 
-	if (array == NULL || size == 0) return NULL;
+	if (array == NULL || size == 0)
+	{
+		return (NULL);
+	}
 
-	for (i = 0; i < size; i++) array[i] = c;
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 
-	return array;
+	return (array);
 }
