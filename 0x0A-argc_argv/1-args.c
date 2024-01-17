76cf146a1416a2eac747df085a3dda0463dbd936
JasmondTheGreat <obafemiolorede@gmail.com>
2024-01-17 01:30:34 +0000

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * prints number of argument passed in
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
