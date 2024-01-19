407fdddd99d81bc472e3b1572b87ebca1e2af9a5
JasmondTheGreat <obafemiolorede@gmail.com>
2024-01-19 19:10:42 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,21 +38,28 @@ char *checkString(char *str)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t len1 = strlen(s1), len2 = n, i;
+	size_t len1, len2, i;
 	size_t totLen;
 	char *result;
 
+
 	if (s1 == NULL)
 		len1 = 0;
+	else
+		for (len1 = 0; s1[len1] != '\0'; len1++);
+
 	if (s2 == NULL)
 		len2 = 0;
+	else
+		for (len2 = 0; s2[len2] != '\0'; len2++);
 
-	if (n >= strlen(s2) && s2 != NULL)
-		len2 = strlen(s2);
+
+	if (len2 > n)
+		len2 = n;
 
 	totLen = len1 + len2 + 1;
 
-	result = (char *)malloc(totLen);
+	result = malloc(sizeof(char) * totLen);
 	if (result == NULL)
 		return (NULL);
 
@@ -66,7 +73,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; i < len2; i++)
 			result[i + len1] = s2[i];
 
-		result[totLen] = '\0';
+		result[len1 + len2] = '\0';
 
 	}
 
