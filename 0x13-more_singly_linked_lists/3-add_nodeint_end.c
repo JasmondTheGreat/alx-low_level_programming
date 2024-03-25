4d66219f575d2895f92513355cc3a2366a7d9d0c
JasmondTheGreat <obafemiolorede@gmail.com>
2024-03-25 02:09:34 +0000

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,18 +19,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 
 
-	if (*head == NULL)
-		*head = new_node;
-	else
-	{
-		while (curr->next != NULL)
-			curr = curr->next;
-
-		curr->next = new_node;
-	}
-
 	new_node->n = n;
 	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	for (curr = *head; curr->next != NULL; curr = curr->next)
+		;
+
 	curr->next = new_node;
 
 	return (new_node);
